1c8b06c8e870d6591cae4bbd7b8fa25b74b91e1d
Awanish Verma <a2023cse9230@imsec.ac.in>
2025-12-30 00:45:08 +0530

    feat: Initialize Express server with API routes and add client build script.

--- server/index.js
+++ server/index.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const path = require('path');
 const cors = require('cors');
 const dotenv = require('dotenv');
 const connectDB = require('./config/db');
@@ -34,6 +35,14 @@ app.use('/api/compiler', require('./routes/compilerRoutes'));
 
 // Error Handling Middleware
 app.use(errorHandler);
+
+// Serve static files from the React app
+app.use(express.static(path.join(__dirname, '../client/dist')));
+
+// Catch-all route to serve the React app
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '../client/dist', 'index.html'));
+});
 
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
